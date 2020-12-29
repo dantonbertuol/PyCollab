82c1e5b92407d1e2bb70e9a373d785396d365afb
dantonbertuol <danton.bertuol@unoesc.edu.br>
2020-12-29 10:20:25 -0300

    Tratamento exceçao resposta WS.

--- CollabMoodle.py
+++ CollabMoodle.py
@@ -14,12 +14,19 @@ if __name__ == "__main__":
         print("Moodle Sesions...")
         moodlSession = ut.leerUUID(param[0])
         for sesion in moodlSession:
-            nombre_session, date_session = webService.get_moodle_sesion_name(sesion)
+            try:
+                nombre_session, date_session = webService.get_moodle_sesion_name(sesion)
+            except:
+                print('Erro WS')
+                nombre_session = None
             if nombre_session == None or nombre_session == ' ':
                 print("Session name not found!")
             else:
                 print(nombre_session)
-                lista_grabaciones = webService.get_moodle_lista_grabaciones(nombre_session, dates, date_session)
+                try:
+                    lista_grabaciones = webService.get_moodle_lista_grabaciones(nombre_session, dates, date_session)
+                except:
+                    lista_grabaciones = None
                 if lista_grabaciones is None:
                     print("There's no recording for: " + nombre_session)
                 else:
